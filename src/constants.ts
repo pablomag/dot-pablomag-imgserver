35a374927aac77cc9a29cfdfc7606f1e6245bb61
pablomag <pablomag@gmail.com>
2020-10-28 13:07:51 +0300

    Type in constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const SERVICE_PORT = process.env.SERVICE_PORT || 3333;
 
 export const API_SECRET = process.env.API_SECRET || "bubblegum_cat";
-export const API_URL = process.env.API_URL ?? "http://localhost:5555";
+export const API_URL = process.env.API_URL || "http://localhost:5555";
 
 export const PROVIDER_URI = process.env.PROVIDER_URI || "https://api.unsplash.com";
 export const PROVIDER_CLIENT_ID = process.env.PROVIDER_CLIENT_ID || "lollipop_tiger";
